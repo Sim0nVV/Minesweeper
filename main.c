87b36aaa49f57a1db6765f2040e678afbd4182d3
Simon <simonvervisch@gmail.com>
2019-11-03 15:33:27 +0100

    Everything finished except non-necessary requirements

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 #include <time.h>
 #include <stdbool.h>
 
-#define  mines  		3
-#define  x_value  		6
-#define  y_value  		6
+#define  MINES  		5
+#define  X_VALUE  		6
+#define  Y_VALUE 		6
 
-#define BOUNDARIES(i,j) 	if (i < 0 || i >= x_value || j < 0 || j >= y_value) { continue;}
+#define BOUNDARIES(i,j) 	if (i < 0 || i >= X_VALUE || j < 0 || j >= Y_VALUE) { continue;}
+int flags_planted = MINES;
+bool dead = false;
 
 struct Field{
    int mines_nearby;
@@ -16,11 +18,12 @@ struct Field{
    bool flag;
 };
 
-struct Field grid[x_value][y_value] = {0, 0, false, false};
+struct Field grid[X_VALUE][Y_VALUE] = {0, 0, false, false};
 
 char return_char_on(int x, int y, bool visible){ // hier moeten type conversions gebeuren
-
-   if(visible || grid[x][y].visible){
+   if (grid[x][y].flag){
+      return 'F';
+   } else if(visible || grid[x][y].visible){
       if (grid[x][y].flag){
 	 return 'F';
       } else if (grid[x][y].mine){
@@ -34,16 +37,16 @@ char return_char_on(int x, int y, bool visible){ // hier moeten type conversions
 
 void print_first_line(){
    printf("   ");
-   for(int i = 0; i<x_value; i++){
+   for(int i = 0; i<X_VALUE; i++){
       printf(" %i  ", i);
    }
    printf("\n");
 }
 
 void print_next_lines(bool visible){
-   for(int y = 0; y<y_value; y++){
+   for(int y = 0; y<Y_VALUE; y++){
       printf("%i ", y);
-      for(int x = 0; x<x_value; x++){
+      for(int x = 0; x<X_VALUE; x++){
 	 printf("| %c|", return_char_on(x,y,visible)); //Hangt ervanaf of het volledige veld zichtbaar is of niet
       }	
       printf("\n");
@@ -51,7 +54,7 @@ void print_next_lines(bool visible){
 }
 
 void print_covered_field() {
-   printf("Resterende vlaggen: %i\n", mines);
+   printf("\nRemaining vlaggen: %i\n\n", flags_planted);
    print_first_line();
    print_next_lines(false); // boolean geeft aan of het hele veld getoond moet worden
 }
@@ -62,11 +65,40 @@ void print_uncovered_field(){
 }
 
 void add_flag(int x,int y){
-   grid[x][y].flag = true;
+   if(flags_planted == 0){
+      printf("Maximum amount of planted flags reached.\n");
+   } else if(grid[x][y].flag) {
+      grid[x][y].flag = !grid[x][y].flag;
+      flags_planted += 1;
+   } else {
+      grid[x][y].flag = !grid[x][y].flag;
+      flags_planted -= 1;
+   }
 }
 
 void reveal(int x, int y){
-   grid[x][y].visible = true;
+   if(!grid[x][y].flag){
+      if(grid[x][y].mines_nearby > 0 && !grid[x][y].mine){
+	 grid[x][y].visible = true;
+      } else if (grid[x][y].mine){
+	 dead = true;
+      } else {
+	 for(int i = x - 1; i < x + 2; i++){ // hij gaat erover. Mag niet over grenzen gaan
+	    for(int j = y - 1; j< y + 2; j++){
+	       BOUNDARIES(i,j)
+		  if(grid[i][j].visible || grid[i][j].mine){
+		     continue;
+		  } else if (grid[i][j].mines_nearby > 0) {
+		     grid[i][j].visible = true;
+		  } else {
+		     grid[i][j].visible = true;
+		     reveal(i,j);
+		  }
+	    }
+	 }
+
+      }
+   }
 }
 
 
@@ -100,36 +132,50 @@ void add_numbers_mine(int x, int y){ //patroon komt ook voor bij onthullen
 	    }
       }
    }
-
+}
+bool all_mines_covered(){
+   bool check = true;
+   for(int x = 0; x< X_VALUE; x++){
+      for(int y = 0; y<Y_VALUE; y++){
+	 if (grid[x][y].mine){
+	    check = check && grid[x][y].flag;
+	 }
+      }
+   }
+   return check;
 }
 
 void initialize_grid(int n){
 
-   int x = rand() % x_value; // code die random vakje zoekt, waar nog geen mijn is
-   int y = rand() % y_value; 
+   int x = rand() % X_VALUE; // code die random vakje zoekt, waar nog geen mijn is
+   int y = rand() % Y_VALUE; 
 
    while(grid[x][y].mine){
-      x = rand() % x_value;
-      y = rand() % y_value;
+      x = rand() % X_VALUE;
+      y = rand() % Y_VALUE;
    }
-   printf("x = %i, y = %i\n", x , y);
 
    grid[x][y].mine = true;
    add_numbers_mine(x, y);
 
-   if(n > 0){
+   if(n > 1){
       initialize_grid(n - 1);
    }
-};
+}
 
 int main()
 {
    srand(time(0));
-   initialize_grid(mines);
+   initialize_grid(MINES);
    char input[6];
-   while (input[0] != 'q' /*|| allvisible*/){
+   while (input[0] != 'q' && !all_mines_covered() && !dead){
       scanf("%[^\n]%*c",input); //waarom geen pointer operator?
       process_command(input);
+   } 
+   if (dead){
+      printf("You Pressed On A Mine. GAME OVER\n");
+   } else if (all_mines_covered()){
+      printf("All Mines Covered With Flags. You Won!\n");
    }
    return 0;
 }
