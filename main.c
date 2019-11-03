e85d39f79710a1d7024ad3662267638b58e8b678
Simon <simonvervisch@gmail.com>
2019-11-03 12:57:10 +0100

    latest git commit

--- main.c
+++ main.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-#define  mines  3
-#define  x_value  5
-#define  y_value  5
+#define  mines  		3
+#define  x_value  		6
+#define  y_value  		6
 
-#define flag_value 2000
-#define visible_value 1000
-#define mine_value -1
+#define BOUNDARIES(i,j) 	if (i < 0 || i >= x_value || j < 0 || j >= y_value) { continue;}
 
-// -1 = Mine,   +1000 = visible, +2000 = flag (Can't happen at same time)
+struct Field{
+   int mines_nearby;
+   int mine;
+   bool visible;
+   bool flag;
+};
 
-int matrix[x_value][y_value] = {0};
+struct Field grid[x_value][y_value] = {0, 0, false, false};
 
+char return_char_on(int x, int y, bool visible){ // hier moeten type conversions gebeuren
 
-char return_char_on(int x, int y){ // hier moeten type conversions gebeuren
-//   printf("%i", matrix[x][y]);
-   
-   if (matrix[x][y] / flag_value == 2 || matrix[x][y] == 1999){
-      return 'F';
-   } else if (matrix[x][y] == mine_value){
-      return 'M';
-   } else if ((matrix[x][y] - 1) <= (9 - 1)){
-      return matrix[x][y] +48;
-   }
-    return '0';
+   if(visible || grid[x][y].visible){
+      if (grid[x][y].flag){
+	 return 'F';
+      } else if (grid[x][y].mine){
+	 return 'M';
+      } else {
+	 return grid[x][y].mines_nearby +48;
+      }}else { 
+	 return ' ';
+      }
 }
 
 void print_first_line(){
@@ -36,15 +40,11 @@ void print_first_line(){
    printf("\n");
 }
 
-void print_next_lines(int visible){
+void print_next_lines(bool visible){
    for(int y = 0; y<y_value; y++){
       printf("%i ", y);
       for(int x = 0; x<x_value; x++){
-	 if (visible){
-	    printf("| %c|", return_char_on(x,y)); //Hangt ervanaf of het volledige veld zichtbaar is of niet
-	 } else {
-	    printf("| %c|", return_char_on(x,y));	
-	 }
+	 printf("| %c|", return_char_on(x,y,visible)); //Hangt ervanaf of het volledige veld zichtbaar is of niet
       }	
       printf("\n");
    }
@@ -53,17 +53,20 @@ void print_next_lines(int visible){
 void print_covered_field() {
    printf("Resterende vlaggen: %i\n", mines);
    print_first_line();
-   print_next_lines(0);
+   print_next_lines(false); // boolean geeft aan of het hele veld getoond moet worden
 }
 
 void print_uncovered_field(){
-   print_covered_field(); //voorlopige code
+   print_first_line();
+   print_next_lines(true); // boolean geeft aan of hele veld geprint moet worden
 }
 
 void add_flag(int x,int y){
-   if (matrix[x][y] % 1000 != 999 ||  ((matrix[x][y] - 1) <= (9 - 1))){
-      matrix[x][y] = matrix[x][y] + flag_value;
-   }
+   grid[x][y].flag = true;
+}
+
+void reveal(int x, int y){
+   grid[x][y].visible = true;
 }
 
 
@@ -71,14 +74,14 @@ void process_command(char *input){
    char command = input[0]; // probleem met uitlezen string
    switch (command){
       case 'P':
-	 print_covered_field();
+	 print_uncovered_field();
 	 break;
       case 'F':
 	 add_flag(input[2] - 48,input[4] - 48); //We geven geen getallen in groter dan 9
 	 print_covered_field();
 	 break;
       case 'R':
-	 //	 reveal(input[2],input[4]);
+	 reveal(input[2] - 48,input[4] - 48);
 	 print_covered_field();
 	 break;
       default:
@@ -89,42 +92,42 @@ void process_command(char *input){
 void add_numbers_mine(int x, int y){ //patroon komt ook voor bij onthullen
    for(int i = x - 1; i < x + 2; i++){ // hij gaat erover. Mag niet over grenzen gaan
       for(int j = y - 1; j< y + 2; j++){
-	 if(matrix[i][j] == mine_value){
-	    continue;
-	 } else {
-	    matrix[i][j] += 1;
-	 }
+	 BOUNDARIES(i,j)
+	    if(grid[i][j].mine){
+	       continue;
+	    } else {
+	       grid[i][j].mines_nearby += 1;
+	    }
       }
    }
 
 }
 
-void initialize_matrix(int n){
-   
-   int x = rand() % x_value;
-   int y = rand() % y_value;
+void initialize_grid(int n){
 
-   while(matrix[x][y] == mine_value){
+   int x = rand() % x_value; // code die random vakje zoekt, waar nog geen mijn is
+   int y = rand() % y_value; 
+
+   while(grid[x][y].mine){
       x = rand() % x_value;
       y = rand() % y_value;
    }
    printf("x = %i, y = %i\n", x , y);
 
-   matrix[x][y] = mine_value;
+   grid[x][y].mine = true;
    add_numbers_mine(x, y);
 
    if(n > 0){
-
-      initialize_matrix(n - 1);
+      initialize_grid(n - 1);
    }
 };
 
 int main()
 {
    srand(time(0));
-   initialize_matrix(mines);
+   initialize_grid(mines);
    char input[6];
-   while (input[0] != 'q'){
+   while (input[0] != 'q' /*|| allvisible*/){
       scanf("%[^\n]%*c",input); //waarom geen pointer operator?
       process_command(input);
    }
