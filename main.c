0f7863a83a4b8e5957218f511b1f424fd1f704f6
Simon Vervisch <44346751+Sim0nVV@users.noreply.github.com>
2019-11-10 22:35:09 +0100

    All functionalities

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #define  Y_VALUE 		6
 
 #define BOUNDARIES(i,j) 	if (i < 0 || i >= X_VALUE || j < 0 || j >= Y_VALUE) { continue;}
+#define INPUT_TO_INT(i)		input[i] - 48
+#define SINGLE_DIGIT(i) 	0 <= i && i <= X_VALUE
 int flags_planted = MINES;
 bool dead = false;
 
@@ -54,7 +56,7 @@ void print_next_lines(bool visible){
 }
 
 void print_covered_field() {
-   printf("\nRemaining vlaggen: %i\n\n", flags_planted);
+   printf("\nRemaining flags: %i\n\n", flags_planted);
    print_first_line();
    print_next_lines(false); // boolean geeft aan of het hele veld getoond moet worden
 }
@@ -103,25 +105,30 @@ void reveal(int x, int y){
 
 
 void process_command(char *input){
-   char command = input[0]; // probleem met uitlezen string
-   switch (command){
-      case 'P':
-	 print_uncovered_field();
-	 break;
-      case 'F':
-	 add_flag(input[2] - 48,input[4] - 48); //We geven geen getallen in groter dan 9
-	 print_covered_field();
-	 break;
-      case 'R':
-	 reveal(input[2] - 48,input[4] - 48);
-	 print_covered_field();
-	 break;
-      default:
-	 printf("Not a known commando\n");
+   if(!((SINGLE_DIGIT(input[2])) && SINGLE_DIGIT(input[4]))){
+      char command = input[0]; // probleem met uitlezen string
+      switch (command){
+	 case 'P':
+	    print_uncovered_field();
+	    break;
+	 case 'F':
+	    add_flag(INPUT_TO_INT(2), INPUT_TO_INT(4)); //We geven geen getallen in groter dan 9
+	    print_covered_field();
+	    break;
+	 case 'R':
+	    reveal(INPUT_TO_INT(2),INPUT_TO_INT(4));
+	    print_covered_field();
+	    break;
+	 default:
+	    printf("Not a known commando\n");
+
+   }
+   } else {
+      printf("You haven't entered digits between 0 and 9");
    }
 }
 
-void add_numbers_mine(int x, int y){ //patroon komt ook voor bij onthullen
+void increment_nearby_cells(int x, int y){ //patroon komt ook voor bij onthullen
    for(int i = x - 1; i < x + 2; i++){ // hij gaat erover. Mag niet over grenzen gaan
       for(int j = y - 1; j< y + 2; j++){
 	 BOUNDARIES(i,j)
@@ -145,37 +152,61 @@ bool all_mines_covered(){
    return check;
 }
 
-void initialize_grid(int n){
+bool all_non_mines_shown(){
+   bool check = true;
+   for(int x = 0; x< X_VALUE; x++){
+      for(int y = 0; y<Y_VALUE; y++){
+	 if (!grid[x][y].mine){
+	    check = check && grid[x][y].visible;
+	 }
+      }
+   }
+   return check;
+}
 
+void initialize_grid(int n, char *input){
+   
    int x = rand() % X_VALUE; // code die random vakje zoekt, waar nog geen mijn is
    int y = rand() % Y_VALUE; 
 
-   while(grid[x][y].mine){
-      x = rand() % X_VALUE;
-      y = rand() % Y_VALUE;
+   while(grid[x][y].mine || (INPUT_TO_INT(2) == x && INPUT_TO_INT(4) == y)){
+      printf("endlessloop \n");
+      int x = rand() % X_VALUE; 
+      int y = rand() % Y_VALUE; 
    }
-
    grid[x][y].mine = true;
-   add_numbers_mine(x, y);
+   // Still bugged
+   increment_nearby_cells(x, y);
 
    if(n > 1){
-      initialize_grid(n - 1);
+      initialize_grid(n - 1, input);
+   } else{
+      process_command(input);
    }
 }
 
 int main()
 {
-   srand(time(0));
-   initialize_grid(MINES);
+   srand(time(NULL));
    char input[6];
-   while (input[0] != 'q' && !all_mines_covered() && !dead){
-      scanf("%[^\n]%*c",input); //waarom geen pointer operator?
+   printf("Insert a Command: R for Reveal, F for Flag, P for show uncovered field\n");
+   scanf("%[^\n]%*c",input); 
+   while(input[0] != 'R' || ((SINGLE_DIGIT(input[2])) && SINGLE_DIGIT(input[4]))){
       process_command(input);
-   } 
+      scanf("%[^\n]%*c",input); 
+   }
+   initialize_grid(MINES, input);
+   do {
+      scanf("%[^\n]%*c",input); 
+      process_command(input);
+   }while (input[0] != 'q' && !all_mines_covered() && !dead && !all_non_mines_shown());
    if (dead){
-      printf("You Pressed On A Mine. GAME OVER\n");
+      printf("You Pressed On A Mine. GAME OVER\n\n");
+      print_uncovered_field();
    } else if (all_mines_covered()){
       printf("All Mines Covered With Flags. You Won!\n");
+   } else if (all_non_mines_shown()){
+      printf("All non mines shown. You Won!\n");
    }
    return 0;
 }
