f33556d1f1b8c6625423161e6ef8f3ede25f99a4
Simon Vervisch <simon.vervisch@vub.be>
2019-12-12 23:23:02 +0100

    Added basic structure for io

---
+++ io.c
@@ -0,0 +1,29 @@
+#include "cell.h"
+/*
+void read_commandline_input(){
+
+// branch naar init struct
+  
+
+// branc naar read txt file
+
+}
+
+void output_curr_iter(){
+
+}
+
+void read_txt_file(){
+
+}
+void free_struct(){
+
+}
+
+
+*/
+void initialize_struct(int w, int h, int m){
+	st = (char *
+
+}
+
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	int arr[2][2] = {0};
+
+	printf("%lu, %lu\n", sizeof(arr[0]), sizeof(int));
+	return 0;
+}
